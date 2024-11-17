acd9491a4d6173abda03acfab5b72b12f94167f9
ademecheklie <ademecheklie78@gmail.com>
2024-11-17 22:44:14 +0300

    adding dream route

--- src/routes/dreamRoute.js
+++ src/routes/dreamRoute.js
@@ -2,7 +2,7 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/dream', (req, res) => {
-  res.status(200).send('');
+  res.status(200).send('My dream is to become a good software engineer and put my legacy in this field.');
 
 });
 
